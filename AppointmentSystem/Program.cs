05a9b59640f536337474cb7e2fc9cc7791567817
Mustafa <Mustafa@DESKTOP-G4JKGDM>
2025-11-20 13:13:39 +0300

    1

--- AppointmentSystem/Program.cs
+++ AppointmentSystem/Program.cs
@@ -61,4 +61,12 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.Run();
